fef236a385626184ff62bf26b134a992294f2715
Kolen Cheung <christian.kolen@gmail.com>
2023-11-22 23:12:46 +0000

    bug fix

--- bin/untar.sh
+++ bin/untar.sh
@@ -4,8 +4,8 @@ ENV_PREFIX="${ENV_PREFIX:-so-conda}"
 BASE_PREFIX="${BASE_PREFIX:-tmp}"
 YYYYMMDD="${YYYYMMDD:-$(date +'%Y%m%d')}"
 
-FILENAME="$(echo "$BASE_PREFIX" | sed 's/\//_/g'))"
-URL="https://github.com/ickc/so-software-environment/releases/download/$YYYYMMDD/$ENV_PREFIX.tar.gz"
+FILENAME="$(echo "$BASE_PREFIX" | sed 's/\//_/g')"
+URL="https://github.com/ickc/so-software-environment/releases/download/$YYYYMMDD/$FILENAME.tar.gz"
 
 cd /tmp || exit 1
 wget "$URL"
